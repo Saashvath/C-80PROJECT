82f06a7be5a9e82885505fff2b5900195c3aa171
Saashvath <74235537+Saashvath@users.noreply.github.com>
2021-01-17 14:43:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+
+var ArrayGetP1= []
+
+var ArrayGetP2= []
+
+function getParagraph1()
+
+
+
+{
+
+    for(var i = 1 ; i <= i++) ;
+
+inputs.push(document.getElementById("para_input_box_1"+i))
+inputs.join(". ")
+document.getElementById("showparagraph1").innerHTML =inputs.join(". ");
+
+document.getElementById("para_input_box_1").value;
+document.getElementById("para_input_box_2").value;
+document.getElementById("para_input_box_3").value;
+document.getElementById("para_input_box_4").value;
+document.getElementById("para_input_box_5").value;  
+document.getElementById("para_input_box_6").value;
+
+}
+
+
+
+
+function getParagraph2()
+{
+
+    for(var b = 1 ; b <= b++) ;q
+
+    inputs.push(document.getElementById("para_input_box_1"+b))
+    inputs.join(". ")
+    document.getElementById("showparagraph2").innerHTML =inputs.join(". ");
+    
+
+    document.getElementById("Input_box1").value;
+    document.getElementById("Input_box2").value;
+    document.getElementById("Input_box3").value;
+    document.getElementById("Input_box4").value;
+    document.getElementById("Input_box5").value;  
+    document.getElementById("Input_box6").value;
+
+
+}   
+
